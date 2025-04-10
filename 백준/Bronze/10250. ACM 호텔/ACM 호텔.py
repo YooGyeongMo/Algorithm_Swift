1b8d423852ada31f1971119df7ce87b8a00f162a
Demian YOO (유경모) <98875580+YooGyeongMo@users.noreply.github.com>
2025-04-11 02:13:01 +0900

    [Bronze III] Title: ACM 호텔, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -3,11 +3,11 @@ T = int(input())
 for test_case in range(T):
     H, W, N = map(int, input().split())
 
-    cnt = 0
+    floor = N%H
+    room_num = N//H + 1
 
-    for i in range(1,W+1):
-        for j in range(1,H+1):
-            floor = j * 100
-            cnt += 1
-            if cnt == N:
-                print(floor+i)
+    if N%H == 0:
+        floor = H
+        room_num = N//H
+
+    print((floor*100) + room_num)
